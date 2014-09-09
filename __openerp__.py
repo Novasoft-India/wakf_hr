8aa1766847b495e79b5455ac5552e4c5a0f8823e
Hashir Haris <hashir.haris@gmail.com>
2014-09-09 09:21:51 +0530

    Wakf HR Module

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+    "name" : "Wakf Board HR",
+    "version" : "1.0",
+    "depends" : ['hr'],
+    "author" : "Novasoft Consultancy Services Pvt. Ltd.",
+    'category' : 'Generic Modules/Others',
+    "description": """Wakf Board - Human Resource & Payroll Modules
+    
+    """,
+    'website': 'http://www.novasoftindia.com',
+    'data': ['earned_leave.xml',],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
